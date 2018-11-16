fc6c352606a7db557120a089c12e6904d920e20d
root <ngomfa@gmail.com>
2018-11-16 15:37:20 +0100

    exo1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie1_Exo1</title>
+  <h1>Créer une variable name et l'initialiser avec la valeur de votre choix.
+    Afficher son contenu.</h1>
+</head>
+<body>
+  <?php
+  $name = "John";
+  // $name nous permet de créer notre variable.
+  echo $name;
+  // echo nous permet d'afficher le contenu de notre variable.
+  ?>
+</body>
+</html>
